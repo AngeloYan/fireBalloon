0de6e9e6d8c6bffc99604425c425fa794ecda992
Hao Jiang <47802415+HaoHalJ@users.noreply.github.com>
2019-03-17 20:51:52 +1100

    Add files via upload

---
+++ 9900_backend/preprocess.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 23:34:09 2019
+
+@author: hal
+"""
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+
+
+def construct_corpus(corpus_raw):
+    new_stopwords = set(stopwords.words('english')) - set(['what', 'which', 'who', \
+                                                           'when', 'where', 'why', \
+                                                           'how','up', 'down','in', \
+                                                           'out', 'on', 'off', 'over', 'under'])
+    
+    corpus = []
+    lemmatizer = WordNetLemmatizer()
+    for x in corpus_raw:
+        one = re.sub('[^a-zA-Z]', ' ', x)
+        one = one.lower()
+        one = one.split()
+        one = [lemmatizer.lemmatize(word, pos='n') for word in one if not word in new_stopwords]
+        one = [lemmatizer.lemmatize(word, pos='v') for word in one]
+        one = ' '.join(one)
+        corpus.append(one)
+        
+    return corpus
