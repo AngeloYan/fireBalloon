f2188a9e4845c36eb37e99e64da3f7ae7deb234a
Angelo Yan <angelo.yan@student.unsw.edu.au>
2019-03-26 17:57:51 +1100

    modified to use heroku

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 const processMessage = require('./process-message');
 
 app.get('/', function (req, res) {
@@ -16,8 +17,8 @@ io.on('connection', function (socket) {
 	});
 });
 
-http.listen(3000, function () {
-	console.log('listening on *:3000');
+http.listen(port, function () {
+	console.log('listening on *:' + port);
 });
 
 async function requestDialogflow(msg) {
