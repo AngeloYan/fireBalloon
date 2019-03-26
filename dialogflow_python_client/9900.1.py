b06dc30a776ec328c61baedf821aa5a333f3c803
yux1020 <z5139047@gmail.com>
2019-03-26 19:54:15 +1100

    update

--- dialogflow_python_client/9900.1.py
+++ dialogflow_python_client/9900.1.py
@@ -1,7 +1,14 @@
 
 from flask import Flask, jsonify, make_response, request
+from IR_model import ir_model
+import requests
 import json
 app = Flask(__name__)
+
+
+#
+# headers = {'content-type':'application/json'}
+# url="http://127.0.0.1:5050/register"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -15,10 +22,14 @@ def webhook():
     query = req.get('queryResult').get('queryText')
 
     intent= req.get('queryResult').get('intent').get('displayName')
-    if intent=='what is v2':
-        intent='ok'
+    if intent=='Default Fallback Intent':
+        # r = requests.post(url, headers=headers)
+        # print(r.text)
+        ans=ir_model(query)
+        # intent='ok'
 
-    print(intent)
+
+    print(ans)
     print(query)
 
     # Check if the request is for the translate action
@@ -32,31 +43,32 @@ def webhook():
     #     output = translate(text, source_lang, target_lang)
     #
     #     # Compose the response to Dialogflow
-    res = {'fulfillmentText': intent}
+    ans = {'fulfillmentText': ans}
                # 'outputContexts': req['queryResult']['outputContexts']}
     # else:
     #     # If the request is not to the translate.text action throw an error
     #     log.error('Unexpected action requested: %s', json.dumps(req))
     #     res = {'speech': 'error', 'displayText': 'error'}
+    print(ans)
 
-    return make_response(jsonify(res))
+    return make_response(jsonify(ans))
 # @app.route('/')
 # def hello_world():
 #     return 'hello world'
 
-@app.route('/register', methods=['POST'])
-def register():
-    # print(request.headers)
-    # print(request.form)
-    # print(request.form['name'])
-    # print(request.form.get('name'))
-    # print(request.form.getlist('name'))
-    # print(request.form.get('nickname', default='little apple'))
-    # #do something else
-    # #
-    # #
-    message ={'fullfilment':''}
-    return message
+# @app.route('/register', methods=['POST'])
+# def register():
+#     # print(request.headers)
+#     # print(request.form)
+#     # print(request.form['name'])
+#     # print(request.form.get('name'))
+#     # print(request.form.getlist('name'))
+#     # print(request.form.get('nickname', default='little apple'))
+#     # #do something else
+#     # #
+#     # #
+#     # message ={'fullfilment':''}
+#     return 'okk'
 
 if __name__ == '__main__':
     app.run(port=8080,debug=True)
