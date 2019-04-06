e3d41049c6ed02c2dc29840609204b740c15866a
HaoHalJ <47802415+HaoHalJ@users.noreply.github.com>
2019-04-06 21:52:20 +1100

    add load_data.py to backend

---
+++ 9900_backend/load_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 12:48:56 2019
+
+@author: hal
+"""
+
+import numpy as np
+
+def load_data(index):
+    if index == 'corpus_data':
+        corpus_data = np.load('corpus_data.npy')
+        corpus_data = list(corpus_data)
+        return corpus_data
+    elif index == 'query_data':
+        query_data = np.load('query_data.npy')
+        query_data = list(query_data)[0]
+        return query_data
+    elif index == 'result_index':
+        result_index = np.load('result_index.npy')
+        result_index = list(result_index)[0]
+        return result_index
