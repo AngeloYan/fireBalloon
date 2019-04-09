be2f8a70e97474b8656c1af8b85a202dc9b8fd4c
Angelo Yan <angelo.yan@student.unsw.edu.au>
2019-04-09 13:21:15 +1000

    moved CSS to new file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const processMessage = require('./process-message');
+
+app.use('/frontend', express.static('frontend'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
