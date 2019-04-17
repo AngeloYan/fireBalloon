619cc8579ec8e1e8b868ea590e1f3583f71677da
yux1020 <z5139047@gmail.com>
2019-04-17 12:26:09 +1000

    Update app.py

--- 9900_backend/app.py
+++ 9900_backend/app.py
@@ -12,20 +12,26 @@ app = Flask(__name__)
 # headers = {'content-type':'application/json'}
 # url="http://127.0.0.1:5050/register"
 
-l1=['comp9444','Monday 6-9pm, Weeks 1-9,11-13 ','Central Lecture Block 7 ',' http://www.cse.unsw.edu.au/~cs9444']
-l2=['comp9414','Wed 10:00 - 13:00 (Weeks:11), Fri 10:00 - 13:00 (Weeks:1-8,10)','Sir John Clancy Auditorium (K-C24-G17)',' http://www.cse.unsw.edu.au/~cs9414']
-l3=['comp1531','Tue 16:00 - 18:00 (Weeks:1-10), Wed 14:00 - 16:00 (Weeks:1-10)','Keith Burrows Theatre (K-J14-G5)',' http://www.cse.unsw.edu.au/~cs1531']
+l1=[['comp9444','9444','neural networks and deep learning'],'Monday 6-9pm, Weeks 1-9,11-13 ','Central Lecture Block 7 ',' http://www.cse.unsw.edu.au/~cs9444']
+l2=[['comp9414','9414','artificial intelligence'],'Wed 10:00 - 13:00 (Weeks:11), Fri 10:00 - 13:00 (Weeks:1-8,10)','Sir John Clancy Auditorium (K-C24-G17)',' http://www.cse.unsw.edu.au/~cs9414']
+l3=[['comp1531','1531','software engineering fundamentals'],'Tue 16:00 - 18:00 (Weeks:1-10), Wed 14:00 - 16:00 (Weeks:1-10)','Keith Burrows Theatre (K-J14-G5)',' http://www.cse.unsw.edu.au/~cs1531']
 obj_1=[l1,l2,l3]
 
 intent_1={}
 save=[None,None]
 def int():
     for i in obj_1:
-        for j in i:
+        for j in i[0]:
+            intent_1[j]='obj'
+        for k in range(1,len(i)):
             intent_1[j]='obj'
     intent_1['time'] = 'int'
-    intent_1['place'] = 'int'
+    intent_1['where'] = 'int'
     intent_1['web'] = 'int'
+    intent_1['when']='int'
+    intent_1['address']='int'
+    intent_1['place']='int'
+    # intent_1['website']='int'
 
 
 @app.route('/webhook', methods=['POST'])
@@ -36,12 +42,17 @@ def webhook():
 
     # Get request parameters
     req = request.get_json(force=True)
-    action = req.get('queryResult').get('action')
+    action = req.get('queryResult')
     query = req.get('queryResult').get('queryText')
-    # context=req.get('queryResult').get('outputContexts')
+    context1=req.get('queryResult').get('outputContexts')
     para=req.get('queryResult').get('parameters')
 
     intent= req.get('queryResult').get('intent').get('displayName')
+
+    print(intent)
+
+
+
 
 
     if intent == 'Default Fallback Intent':
@@ -62,7 +73,16 @@ def webhook():
         ans4 = {'fulfillmentText': ans4}
         return make_response(jsonify(ans4))
 
-    if intent == 'place' or 'timetable' or 'course web site':
+    if intent == 'place' or 'timetable' or 'course_web_site':
+        check=query.split()
+        for i in range(0 ,len(check)):
+            if 'comp' in check[i]:
+                if check[i] != 'comp9444' and  check[i] !='comp9414' and check[i] !='comp1531':
+                    ans5='Sorry, we do not have information about this course'
+                    ans5={'fulfillmentText': ans5}
+                    return make_response(jsonify(ans5))
+                else:
+                    break
         ans2 = context(query)
         ans2 = {'fulfillmentText': ans2}
         return make_response(jsonify(ans2))
@@ -114,6 +134,7 @@ def webhook():
 #     return 'okk'
 
 def context(query):
+    query=query.lower()
     int()
     for key in intent_1:
         if  key in query:
@@ -124,13 +145,13 @@ def context(query):
     if save[0] is None:
         return 'what do you want to know about it'
     if save[1] is None:
-        return 'what course do you ask'
+        return 'what course do you want to know about?'
     for i in obj_1:
-        for j in i:
+        for j in i[0]:
             if j == save[1]:
-                if save[0] == 'time':
+                if save[0] == 'time' or save[0] =='when':
                     return i[1]
-                elif save[0] == 'place':
+                elif save[0] == 'where' or save[0] =='place' or save[0] =='address':
                     return i[2]
                 elif save[0] == 'web':
                     return i[3]
@@ -140,7 +161,4 @@ def context(query):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-    # app.run(port=8080,debug=True)
+    app.run(port=8080,debug=True)
