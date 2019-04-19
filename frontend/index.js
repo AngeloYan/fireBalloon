c6d0f1837af3914e08119c308e5b7b21d09fc6ec
Angelo Yan <angelo.yan@student.unsw.edu.au>
2019-04-19 19:40:05 +1000

    extract pdf name

--- frontend/index.js
+++ frontend/index.js
@@ -4,3 +4,7 @@ function loadPage() {
     document.getElementById("submitFormInput").style.width =
         (totalWidth - buttonWidth - 20).toString() + "px";
 }
+
+function openPDF(pdfName) {
+    document.getElementById("pdfDisplay").src = pdfName;
+}
